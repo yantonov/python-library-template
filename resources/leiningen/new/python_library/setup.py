1c085dc125d92a8ef6d15e9d99d3983b348cdd83
Yury Antonov <yantonov@yandex.ru>
2020-04-10 00:17:14 +0500

    add placeholder for required dependencies

--- resources/leiningen/new/python_library/setup.py
+++ resources/leiningen/new/python_library/setup.py
@@ -12,11 +12,16 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
+
     packages=setuptools.find_packages(exclude=('tests')),
+        python_requires='>=3.6',
+    install_requires=[],
+    extras_require={"tests": ["pytest"]},
+
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+
 )
